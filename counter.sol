bbfa600804b04da8b36ccccef8e6717997066ec8
trangchongcheng <trangchongcheng@users.noreply.github.com>
2024-05-10 10:01:06 +0700

    Update counter.sol

--- counter.sol
+++ counter.sol
@@ -9,3 +9,22 @@ interface IGmpReceiver {
         bytes calldata payload
     ) external payable returns (bytes32);
 }
+contract Counter is IGmpReceiver {
+    address private immutable _gateway;
+    uint256 public number;
+
+    constructor(address gateway) {
+        _gateway = gateway;
+    }
+
+    function onGmpReceived(
+        bytes32,
+        uint128,
+        bytes32,
+        bytes calldata
+    ) external payable override returns (bytes32) {
+        require(msg.sender == _gateway, "unauthorized");
+        number++;
+        return bytes32(number);
+    }
+}
